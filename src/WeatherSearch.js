3bde301f4798ca195ce50f15de5f61e330442ac6
iriscuello <64563326+iriscuello@users.noreply.github.com>
2020-12-07 20:49:37 -0500

    added icon and new useState for city
    created new function for the api call

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -4,14 +4,14 @@ import WeatherInfo from "./WeatherInfo";
 
 export default function SearchEngine(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState (props.cityDefault);
 
   function tempDisplay(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
       date: new Date(response.data.dt * 1000),
       iconURL:
-        "https://d29fhpw069ctt2.cloudfront.net/icon/image/84559/preview.svg",
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       temperature: response.data.main.temp,
       realFeel: Math.round(response.data.main.feels_like),
       hiTemp: Math.round(response.data.main.temp_max),
@@ -23,10 +23,24 @@ export default function SearchEngine(props) {
     });
   }
 
+  function search() {
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f6daddd2490e280fc02eb01a9840f82a&units=metric`;
+    axios.get(url).then(tempDisplay);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+   setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form id="search-form" className="mb-3">
+        <form onSubmit={handleSubmit} id="search-form" className="mb-3">
           <div className="row">
             <div className="col-9">
               <input
@@ -35,6 +49,7 @@ export default function SearchEngine(props) {
                 className="form-control"
                 id="searchEngine"
                 autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -43,6 +58,7 @@ export default function SearchEngine(props) {
                 value="Let's See"
                 id="button"
                 className="btn btn-primary w-100"
+                
               />
             </div>
           </div>
@@ -51,10 +67,6 @@ export default function SearchEngine(props) {
       </div>
     );
   } else {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.cityDefault}&appid=f6daddd2490e280fc02eb01a9840f82a&units=metric`;
-
-    axios.get(url).then(tempDisplay);
-
     return "Loading...";
   }
 }
