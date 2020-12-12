e806405ca9699265a0c82d07bc583b2349f92c7b
iriscuello <64563326+iriscuello@users.noreply.github.com>
2020-12-11 19:18:52 -0500

    conversion of temp unit complete

--- src/TempConversion.js
+++ src/TempConversion.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function TempConversion () { return (
+export default function TempConversion (props) { 
+  const [unit, setUnit]  = useState("celsius");
+  function conversionToFar (event) {
+   event.preventDefault();
+    setUnit("fahrenheit");
+  }
+function conversionToCel (event) {
+  event.preventDefault();
+  setUnit("celsius");
+}
+
+  if(unit === "celsius") {
+   return (
 <ul id="current">
   <li>
-    <span id="temperature">{Math.round(props.data.temperature)}</span>
-    <span id="cel">℃</span>
-    <span id="line">|</span> <span id="fer">℉</span>
+    <span id="temperature">{Math.round(props.celsius)}</span>
+    <span className="unit" id="cel">℃ | <a href="/" onClick={conversionToFar}>℉</a></span>
   </li>
-  <li id="realFeel">Feels Like: {props.data.realFeel}℃</li>
-  <li id="hiTemp">HI: {props.data.hiTemp}℃</li>
-  <span id="lowTemp">LOW: {props.data.lowTemp}℃ </span>
 </ul>
-);
+); 
+  }
+  else { 
+    let far = (props.celsius * 9/5) + 32;
+    return (
+      <ul id="current">
+        <li>
+          <span id="temperature">{Math.round(far)}</span>
+          <span className="unit" id="cel">
+            <a href="/" onClick={conversionToCel}>℃ |</a>{" "}
+              ℉
+          </span>
+        </li>
+      </ul>
+    );
+  }
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -9,35 +9,27 @@ export default function WeatherInfo(props) {
         <h1 id="city">{props.data.city}</h1>
         <DateTime date={props.data.date} />
         <div className="row">
-          <TempConversion celsius={props.data.temperature} />
           <div className="col-6">
-            <ul id="current">
-              <li>
-                <span id="temperature">
-                  {Math.round(props.data.temperature)}
-                </span>
-                <span id="cel">℃</span>
-                <span id="line">|</span> <span id="fer">℉</span>
-              </li>
+            <ul>
+              <TempConversion celsius={props.data.temperature} />
               <li id="realFeel">Feels Like: {props.data.realFeel}℃</li>
               <li id="hiTemp">HI: {props.data.hiTemp}℃</li>
               <span id="lowTemp">LOW: {props.data.lowTemp}℃ </span>
             </ul>
           </div>
           <div className="col-6" id="image">
+            <WeatherIcon code={props.data.icon} />
             <div>
-            <WeatherIcon code={props.data.icon}
-              />
-             </div>
-            <ul id="listWH">
-              <li id="windSpeed">Wind Speed: {props.data.wind} km/h</li>
-              <li id="humidity">Humidity: {props.data.humidity}%</li>
-              <li className="text-capitalize" id="description">
-                {props.data.description}
-              </li>
-            </ul>
+              <ul id="listWH">
+                <li id="windSpeed">Wind Speed: {props.data.wind} km/h</li>
+                <li id="humidity">Humidity: {props.data.humidity}%</li>
+                <li className="text-capitalize" id="description">
+                  {props.data.description}
+                </li>
+              </ul>
+            </div>
           </div>
-          </div>
+        </div>
       </div>
     );
 }
